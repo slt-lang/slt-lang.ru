46bc2c7db70a4b65d91e144935005cccfb586e79
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2024-09-17 20:51:55 +0300

    больше рефлексии

--- sltlang/Controllers/SpecificationController.cs
+++ sltlang/Controllers/SpecificationController.cs
@@ -11,16 +11,27 @@ namespace sltlang.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ILocaleService _locale;
-        public static readonly ConcurrentDictionary<string, Type> Types = new ConcurrentDictionary<string, Type>(new Dictionary<string, Type>()
+        public static readonly ConcurrentDictionary<string, Type> TypesWithArticle = new ConcurrentDictionary<string, Type>(new Dictionary<string, Type>()
         {
             { "baseexpression", typeof(BaseExpression) },
             { "binaryoperator", typeof(BinaryOperator) },
         });
-        public static readonly Type[] DefaultTypes = new Type[]
+        public static readonly Type[] SupportedTypes = GetSupportedTypes();
+
+        private static Type[] GetSupportedTypes()
         {
-            typeof(SLThree.ExecutionContext),
-        };
-        private static readonly ConcurrentDictionary<string, Type> dtypes = new ConcurrentDictionary<string, Type>(DefaultTypes.Select(x => new KeyValuePair<string, Type>(x.Name.ToLower(), x)));
+            var l = new List<Type>();
+            var types = typeof(BaseExpression).Assembly.GetTypes();
+            l.Add(typeof(SLThree.ExecutionContext));
+            l.Add(typeof(SLThree.ExecutionContext.IExecutable));
+            l.Add(typeof(SLThree.ContextWrap));
+            l.Add(typeof(SLThree.SourceContext));
+            l.AddRange(types.Where(x => x.IsType(typeof(BaseExpression))));
+            l.AddRange(types.Where(x => x.IsType(typeof(BaseStatement))));
+            return l.Distinct().ToArray();
+        }
+
+        private static readonly ConcurrentDictionary<string, Type> TypesWithoutArticle = new ConcurrentDictionary<string, Type>(SupportedTypes.Select(x => new KeyValuePair<string, Type>(x.Name.ToLower(), x)));
 
         public SpecificationController(ILogger<HomeController> logger, ILocaleService locale)
         {
@@ -36,10 +47,10 @@ namespace sltlang.Controllers
             if (_locale.Locales.ContainsKey(Language))
             {
                 ViewData["culture"] = _locale.Locales[Language];
-                if (Types.ContainsKey(article))
-                    return View(article, Types[article]);
-                if (dtypes.ContainsKey(article))
-                    return View(dtypes[article]);
+                if (TypesWithArticle.ContainsKey(article))
+                    return View(article, TypesWithArticle[article]);
+                if (TypesWithoutArticle.ContainsKey(article))
+                    return View(TypesWithoutArticle[article]);
                 //if (Article.ExecutablesSpecification.ContainsKey(article))
                 //    return View(Article.ExecutablesSpecification[article]);
                 return ArticleNotFound(article);
