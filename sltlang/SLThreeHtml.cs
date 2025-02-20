70bf8b1ca25c8ea1576a374124ed82cf8cb0cd16
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-02-20 16:53:35 +0300

    composition

--- sltlang/SLThreeHtml.cs
+++ sltlang/SLThreeHtml.cs
@@ -5,47 +5,65 @@ using SLThree.Metadata;
 
 namespace sltlang
 {
-    public class Pizdec<T> : T where T: DefaultRestorator
-    {
-
-    }
-
     public class SLThreeHtml : Restorator
     {
-        private static string OpenTag(string tag, string @class) => $"<{tag}{(@class != null ? $" class=\"{@class}\"" : "")}>";
-        private static string CloseTag(string tag) => $"</{tag}>";
-        private static string Html(string str, string tag, string @class)
+        public SLThreeHtml() : base()
         {
-            return $"{OpenTag(tag, @class)}{str.Replace("<", "&lt;").Replace(">", "&gt;")}{CloseTag(tag)}";
+            Writer = new HtmlWriter();
         }
 
-        public override void WriteCallText(string s)
+        public class HtmlWriter : DefaultRestoratorWriter
         {
-            sb.Append(Html(s, "span", "slt-call"));
-        }
-        public override void WriteExpressionKeyword(string s)
-        {
-            sb.Append(Html(s, "span", "slt-keyword1"));
-        }
-        public override void WriteStatementKeyword(string s)
-        {
-            sb.Append(Html(s, "span", "slt-keyword2"));
-        }
-        public override void WriteTab()
-        {
-            sb.Append(new string(' ', Tabulation * Level).Replace(" ", "&nbsp;"));
-        }
-        public override void WriteTypeText(string s)
-        {
-            sb.Append(Html(s, "span", "slt-type"));
-        }
-        public override void WritePlainText(string s)
-        {
-            sb.Append(Html(s, "span", "slt-plain"));
-        }
-        public override void WritelnPlainText(string s)
-        {
-            sb.AppendLine(Html(s, "span", "slt-plain"));
+            private static string OpenTag(string tag, string @class) => $"<{tag}{(@class != null ? $" class=\"{@class}\"" : "")}>";
+            private static string CloseTag(string tag) => $"</{tag}>";
+            private static string Html(string str, string tag, string @class)
+            {
+                return $"{OpenTag(tag, @class)}{str.Replace("<", "&lt;").Replace(">", "&gt;")}{CloseTag(tag)}";
+            }
+
+            public override void WriteCallText(string s)
+            {
+                Sb.Append(Html(s, "span", "slt-call"));
+            }
+            public override void WriteExpressionKeyword(string s)
+            {
+                Sb.Append(Html(s, "span", "slt-keyword1"));
+            }
+            public override void WriteStatementKeyword(string s)
+            {
+                Sb.Append(Html(s, "span", "slt-keyword2"));
+            }
+            public override void WriteTab()
+            {
+                Sb.Append(new string(' ', Tabulation * Level).Replace(" ", "&nbsp;"));
+            }
+            public override void WriteTypeText(string s)
+            {
+                Sb.Append(Html(s, "span", "slt-type"));
+            }
+            public override void WritePlainText(string s)
+            {
+                Sb.Append(Html(s, "span", "slt-plain"));
+            }
+            public override void WritelnPlainText(string s)
+            {
+                Sb.AppendLine(Html(s, "span", "slt-plain"));
+            }
+
+            public override void WriteStringText(string s)
+            {
+                Sb.AppendLine(Html(s, "span", "slt-string"));
+            }
+
+            public override void WriteErrorText(string s)
+            {
+                Sb.AppendLine(Html(s, "span", "slt-error"));
+            }
+
+            public void WriteDigitText(string s)
+            {
+                Sb.AppendLine(Html(s, "span", "slt-digit"));
+            }
         }
     }
 }
