d740687f26a0751248dc996b9eba04ef2d1031dd
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2025-04-18 07:05:52 +0300

    server state

---
+++ sltlang.Common/Common/ServiceInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sltlang.Common.Common
+{
+    public class ServiceInfo
+    {
+        public DateTime DeployTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Version { get; set; } = default!;
+
+        public static ServiceInfo GetServiceInfo(Assembly assembly)
+        {
+            return new ServiceInfo()
+            {
+                DeployTime = File.GetLastWriteTime(assembly.Location),
+                StartTime = Process.GetCurrentProcess().StartTime,
+                Version = assembly.GetName().Version!.ToString(3),
+            };
+        }
+    }
+}
---
+++ sltlang.Domain/Logic/ServiceInfoLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sltlang.Common.Common;
+using sltlang.Domain.Ports;
+
+namespace sltlang.Domain.Logic
+{
+    public class ServiceInfoLogic(Config config, IHttpService httpService) : IServiceInfoLogic
+    {
+        public async Task<IDictionary<string, ServiceInfo>> GetServiceInfos()
+        {
+            var ret = new ConcurrentDictionary<string, ServiceInfo>(config.PeerServices.Keys.ToDictionary(x => x, x => default(ServiceInfo))!);
+
+            var requests = config.PeerServices.Keys.Select<string, Func<Task>>(x => async () =>
+                {
+                    try
+                    {
+                        var (success, response) = await httpService.JsonRequest<object, ServiceInfo>(x, "serviceinfo", [], HttpMethod.Get, null, TimeSpan.FromSeconds(1));
+                        if (success)
+                        {
+                            ret[x] = response!;
+                        }
+                    }
+                    catch { }
+                }).Select(x => x()).ToArray();
+
+            await Task.WhenAll(requests);
+
+            return ret;
+        }
+    }
+}
